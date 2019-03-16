a91a6e22322039f0982646d82e91e825a0fdc347
maydial <31964881+maydial@users.noreply.github.com>
2019-03-15 21:48:25 -0400

    Update TEST2.java

--- TEST2.java
+++ TEST2.java
@@ -1,107 +1,67 @@
 
 	
-	public class TEST2 {
-
-static String text = "+-000-+0+-+-00000+0" ;
-
-static String bonTruc = "" ;
-
-static String bonTruc1 = "" ;
-
-　
-
-static public void main(String[] args) {
-
-//System.out.println(text.charAt(i)) ;
-
-//System.out.println(text.charAt(i+1)) ;
-
-System.out.println(text);
-
-System.out.println(text.replace("000+-0-+","00000000") );
-
-bonTruc = text.replace("000+-0-+","00000000") ;
-
-System.out.println(containsChar(bonTruc, '-'));
-
-bonTruc1 = containsChar(bonTruc, '-') ;
-
-System.out.println("-------------------------") ;
-
-System.out.println(replacesign (bonTruc1 ) );
-
-}
-
-public static String replacesign (String s ) {
-
-String good = "" ;
-
-for (int i=0; i<s.length() ;){
-
-if (s.charAt(i) == '+' ){
-
-//System.out.println(s.replace("+","1"));
-
-good= s.replace("+","1") ;
-
-}
-
-break ;
-
-}
-
-return good;
-
-}
-
-　
-
-　
-
-public static String containsChar(String s, char search) {
-
-String good = "" ;
-
-for (int i=0; i<s.length() ;i++){
-
-//	System.out.println(s.charAt(i)) ;
-
-//i++ ;
-
-try {
-
-if (s.charAt(i) == '-') {
-
-//System.out.println(s.replace("-","1"));
-
-good = s.replace("-","1") ;
-
-}}
-
-catch (IndexOutOfBoundsException e) {
-
-// this should never happen...
-
-}
-
-}
-
-return good;
-
-　
-
-　
-　
-
-　
-
-}
-
-//if (s.charAt(i) == '+') {
-
-//System.out.println(s.replace("+","1"));
-
-//good = s.replace("+","1") ;
-
-}
-
+	
+public class TEST2 {
+	
+	 static public void main(String[] args) { 
+		    // String  text = "+000+-0-+" ;
+	    	//String  text = "+000+-0-+000-00" ;
+	    	//String  text = "+-000-+0+-+-00000+0";
+	    	String  text = "+-0+00-" ;
+	    	String bonTruc = "" ;
+	    	String a = "" ;
+	    	String b = "" ;
+	    	String c = "" ;
+	    	 
+	    
+	
+				
+					
+				 
+					//System.out.println(text.charAt(i)) ;
+					//System.out.println(text.charAt(i+1)) ;
+					//System.out.println(text);
+					
+					a = text.replace("000+-0-+","00000000") ;
+					//System.out.println("a est :"+a);
+					bonTruc = a.replace("000-+0+-","00000000") ;
+					//System.out.println("bon truc est :"+bonTruc);
+				
+					
+				
+					
+					for (int i=0; i<bonTruc.length() ;i++){
+						
+				    	if (bonTruc.charAt(i) ==  '+'  ){
+				    		b =bonTruc.replace("+","1") ;
+				    		
+				    	
+				    	}	
+				    	
+				    	
+					}
+					  for (int i=0; i<b.length() ;i++){
+						
+				    	 if  (b.charAt(i)==  '-' ){
+				    		c =b.replace("-","1") ;
+				    		
+				    	}
+				    	 
+				    	
+				    		
+				   
+					}	
+					  System.out.println(bonTruc.replace("+","1")); 
+					  System.out.println(c); 
+	 
+	 
+	 } 
+				
+		
+				   
+		    
+	  
+	    
+	
+			} 
+			
