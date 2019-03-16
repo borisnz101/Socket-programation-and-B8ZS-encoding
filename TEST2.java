4599ec0e35408c91c714352cee386cc596380f0e
maydial <31964881+maydial@users.noreply.github.com>
2019-03-16 09:31:31 -0400

    Update TEST2.java

--- TEST2.java
+++ TEST2.java
@@ -1,13 +1,14 @@
 
 	
 	
+
 public class TEST2 {
 	
 	 static public void main(String[] args) { 
 		    // String  text = "+000+-0-+" ;
 	    	//String  text = "+000+-0-+000-00" ;
-	    	//String  text = "+-000-+0+-+-00000+0";
-	    	String  text = "+-0+00-" ;
+	    	String  text = "+-000-+0+-+-00000+0";
+	    	//String  text = "+-0+00-" ;
 	    	String bonTruc = "" ;
 	    	String a = "" ;
 	    	String b = "" ;
@@ -32,26 +33,27 @@ public class TEST2 {
 					
 					for (int i=0; i<bonTruc.length() ;i++){
 						
-				    	if (bonTruc.charAt(i) ==  '+'  ){
+				   // 	if (bonTruc.charAt(i) ==  '+' &  bonTruc.charAt(i)==  '-' ){
 				    		b =bonTruc.replace("+","1") ;
-				    		
-				    	
-				    	}	
-				    	
-				    	
-					}
-					  for (int i=0; i<b.length() ;i++){
-						
-				    	 if  (b.charAt(i)==  '-' ){
 				    		c =b.replace("-","1") ;
 				    		
-				    	}
+				    	
+				    //	}	
+				    	
+				    	
+					//}
+					//  for (int i=0; i<b.length() ;i++){
+						
+				    	// if  (b.charAt(i)==  '-' ){
+				    	//	c =b.replace("-","1") ;
+				    		
+				   // 	}
 				    	 
 				    	
 				    		
 				   
 					}	
-					  System.out.println(bonTruc.replace("+","1")); 
+					 // System.out.println(bonTruc.replace("+","1")); 
 					  System.out.println(c); 
 	 
 	 
@@ -64,4 +66,3 @@ public class TEST2 {
 	    
 	
 			} 
-			
